158f968624392f46ed8bf4124ad7fb4dd43fa27e
Richard Guay <raguay@customct.com>
2024-06-26 21:01:42 +0700

    Creating skelaton for preferences

---
+++ frontend/src/components/BoardPref.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from "svelte";
+  import { boardCursor } from "../stores/boardCursor.js";
+  import { preferences } from "../stores/preferences.js";
+  import { Kanban } from "../stores/Kanban.js";
+</script>
+
+<h2>Board Preferences</h2>
+
+<style>
+  h2 {
+    text-align: center;
+  }
+</style>
---
+++ frontend/src/components/ItemPref.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount, tick } from "svelte";
+  import { boardCursor } from "../stores/boardCursor.js";
+  import { listCursor } from "../stores/listCursor.js";
+  import { itemCursor } from "../stores/itemCursor.js";
+  import { preferences } from "../stores/preferences.js";
+  import { Kanban } from "../stores/Kanban.js";
+</script>
+
+<h2>Item Preferences</h2>
+
+<style>
+  h2 {
+    text-align: center;
+  }
+</style>
